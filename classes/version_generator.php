b19570ae28e5c87b939f0e1aaf1261c7dac87cb7
Alexandru Elisei <alexandru.elisei@gmail.com>
2016-06-10 16:21:17 +0300

    Add version_generator class and PHPUnit tests
    The class will generate the version.php file

---
+++ classes/version_generator.php
@@ -0,0 +1,92 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing the version_generator class.
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/classes/component.php');
+require_once(__DIR__ . '/generator_base.php');
+require_once(__DIR__ . '/processor.php');
+
+/**
+ * Version_generator class.
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_pluginkenobi_version_generator extends tool_pluginkenobi_generator_base {
+    /** @var string[] Moodle versions. */
+    protected $moodleversions = array(
+        '3.1' => '2016052300',
+        '3.0' => '2015111600',
+        '2.9' => '2015051100',
+        '2.8' => '2014111000',
+        '2.7' => '2014051200',
+        '2.6' => '2013111800',
+        '2.5' => '2013051400',
+        '2.4' => '2012120300',
+        '2.3' => '2012062500',
+        '2.2' => '2011120500'
+    );
+
+    /** @var string[] Valid maturity levels. */
+    protected $maturitylevels = array('MATURITY_ALPHA', 'MATURITY_BETA', 'MATURITY_RC', 'MATURITY_STABLE');
+
+    /** @var $string[] List of features that the plugin has. */
+    protected $features = array(
+        'core'      => array(
+            'requiredoptions'   => array('component', 'version', 'requires'),
+            'optionaloptions'   => array('release', 'maturity', 'dependencies'),
+            'files'             => array(
+                'skel/version' => 'version.php'
+            )),
+    );
+
+    /**
+     * Checks if a given option is valid.
+     *
+     * @param string $option The option to be validated.
+     * @return string | null The validated option value or null if it's not a valid value.
+     */
+    protected function validate_value($option, $value) {
+        if ($option == 'requires') {
+            if (!isset($this->moodleversions[$value])) {
+                return null;
+            }
+            return $this->moodleversions[$value];
+        }
+
+        if ($option == 'maturity') {
+            if (!in_array($value, $this->maturitylevels)) {
+                return null;
+            }
+        }
+
+        if (empty($value)) {
+            return null;
+        } else {
+            return $value;
+        }
+    }
+}
---
+++ tests/version_generator_test.php
@@ -0,0 +1,101 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing tests for the version_generator class.
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei alexandru.elisei@gmail.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+global $CFG;
+require_once($CFG->libdir . '/setuplib.php');
+
+/**
+ * version_generator test class.
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei alexandru.elisei@gmail.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_pluginkenobi_version_generator_testcase extends advanced_testcase {
+
+    /** @var string[] Basic recipe. */
+    protected static $baserecipe = array(
+        'component' => 'test_versiongeneratortest',
+        'name'      => 'version_generator test',
+        'release'   => '0.1',
+        'year'      => 2016,        // For the boilerplate.
+        'author'    => array(
+            'name'  => 'Alexandru Elisei',
+            'email' => 'alexandru.elisei@gmail.com'
+        ),
+        'version'   => '2016121200',
+        'requires'  => '2.9',
+    );
+
+    /** @var string Fixture locations. */
+    protected static $fixtures;
+
+    /**
+     * Sets the fixture location.
+     */
+    public static function setUpBeforeClass() {
+        global $CFG;
+
+        self::$fixtures = $CFG->dirroot . '/admin/tool/pluginkenobi/tests/fixtures/version_generator';
+    }
+
+    /**
+     * Tidy up open files that may be left open.
+     */
+    protected function tearDown() {
+        gc_collect_cycles();
+    }
+
+    /**
+     * Tests generating a version.php file with missing required options.
+     */
+    public function test_missing_options() {
+        $recipe = self::$baserecipe;
+        unset($recipe['version']);
+
+        $this->setExpectedException('moodle_exception');
+        $generator = new tool_pluginkenobi_version_generator($recipe, '');
+    }
+
+    /**
+     * Tests generating a version.php file with options.
+     */
+    public function test_options() {
+        $recipe = self::$baserecipe;
+        $recipe['features'] = array(
+            'all' => false
+        );
+        $recipe['maturity'] = 'MATURITY_ALPHA';
+        $targetdir = make_request_directory() . '/';
+
+        $generator = new tool_pluginkenobi_version_generator($recipe, $targetdir);
+        $generator->generate_files();
+
+        $versionfile = $targetdir . '/versiongeneratortest/version.php';
+        $this->assertFileExists($versionfile);
+        $this->assertFileEquals($versionfile, self::$fixtures . '/version_generator_test_options.php');
+    }
+}
