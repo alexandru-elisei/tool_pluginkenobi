bb5deef5713d8014a66d061acbad8da8cf98c2c7
Alexandru Elisei <alexandru.elisei@gmail.com>
2016-06-10 16:08:34 +0300

    Add recipe_processor class
    The class will process the recipe created by the user and return a set
    of options extracted from the recipe.

---
+++ classes/recipe_processor.php
@@ -0,0 +1,77 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing the recipe_processor class
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(__DIR__ . '/../Spyc/Spyc.php');
+require_once(__DIR__ . '/../../../../config.php');
+
+/**
+ * Recipe_processor class.
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_pluginkenobi_recipe_processor {
+    /**
+     * Process and return the recipe file.
+     *
+     * The filename is absolute and must include the '.yaml' extension.
+     *
+     * @throws moodle_exception.
+     * @param string $filename The yaml file.
+     */
+    public static function load($filename) {
+        try {
+            $data = Spyc::YAMLLoad($filename);
+        } catch (Exception $e) {
+            throw new moodle_exception('Error loading file "' . $filename . '": ' . $e->getMessage());
+        }
+
+        // Extracting author name and email.
+        if (!empty($data['author']) && is_array($data['author'])) {
+            foreach ($data['author'] as $entry) {
+                if (!empty($entry['name'])) {
+                    $data['author']['name'] = $entry['name'];
+                } else if (!empty($entry['email'])) {
+                    $data['author']['email'] = $entry['email'];
+                }
+            }
+        }
+
+        // Extracting requested features.
+        $requestedfeatures = array();
+        if (!empty($data['features']) && is_array($data['features'])) {
+            foreach ($data['features'] as $entry) {
+                $value = reset($entry);
+                $option = key($entry);
+                $requestedfeatures[$option] = $value;
+            }
+        }
+        $data['features'] = $requestedfeatures;
+
+        return $data;
+    }
+}
