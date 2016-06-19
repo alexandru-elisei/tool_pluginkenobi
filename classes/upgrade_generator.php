368eaf0eaf6daba9d43f1bf35c87b10d07055880
Alexandru Elisei <alexandru.elisei@gmail.com>
2016-06-19 22:37:37 +0300

    Add upgrade_generator class
    Tracker issue https://tracker.moodle.org/browse/CONTRIB-6400

---
+++ classes/upgrade_generator.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * File containing the upgrade_generator class.
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/classes/component.php');
+require_once(__DIR__ . '/helper_generator_base.php');
+require_once(__DIR__ . '/processor.php');
+
+/**
+ * Upgrade_generator class.
+ *
+ * @package    tool_pluginkenobi
+ * @copyright  2016 Alexandru Elisei
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_pluginkenobi_upgrade_generator extends tool_pluginkenobi_helper_generator_base {
+    /** @var $string[] List of features that the plugin has. */
+    protected $features = array(
+        'core'  => array(
+            'requiredoptions'   => array(),
+            'optionaloptions'   => array('upgradelib'),
+            'files'             => array(
+                'db/upgrade.php'  => array('template' => 'skel/db/upgrade')
+            )),
+    );
+
+    protected $implementedfeature = 'upgrade';
+
+    /**
+     * Adds the upgradelib.php file if the option was specified.
+     *
+     * @param string[] $recipe The recipe.
+     */
+    protected function execute_additional_steps($recipe) {
+        if (!empty($this->recipe['features']['upgrade']['upgradelib']) &&
+                $this->recipe['features']['upgrade']['upgradelib'] === true) {
+            $this->outputfiles['db/upgradelib.php'] = array('template' => 'skel/db/upgradelib');
+        }
+    }
+}
